438a417127253c8527dc144464dccec0b7e98708
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-15 20:43:59 -0400

    Add AutoLotContext class to AutoLot data access layer.

---
+++ AutoLotDAL_Core2/EF/AutoLotContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AutoLotDAL_Core2.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace AutoLotDAL_Core2.EF
+{
+    public class AutoLotContext : DbContext
+    {
+        public AutoLotContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = @"server=(localdb)\mssqllocaldb;database=AutoLotCore2;integrated security=true;MultipleActiveResultSets=true;App=EntityFramework;";
+                optionsBuilder.UseSqlServer(connectionString, options => options.EnableRetryOnFailure());
+            }
+        }
+    }
+}
