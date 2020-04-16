f3c0bf840eae22aa9ea8f2e66aacddb36a0c520b
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-16 09:07:22 -0400

    Add MyDataInitializer class to AutoLotDAL project

---
+++ AutoLotDAL_Core2/DataInitialization/MyDataInitializer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using AutoLotDAL_Core2.EF;
+using AutoLotDAL_Core2.Models;
+using Microsoft.EntityFrameworkCore;
+namespace AutoLotDAL_Core2.DataInitialization
+{
+    public static class MyDataInitializer
+    {
+    }
+}
